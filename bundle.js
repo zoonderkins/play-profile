7030657f4c012ea96371b96a127d294e5c7831fc
ookangzheng <okz5289@gmail.com>
2019-05-15 01:28:17 +0800

    fix route pathname when URL can't solve with /

--- bundle.js
+++ bundle.js
@@ -90,7 +90,7 @@ module.exports = () => {
 const filesystem = require('./filesystem')
 const demo = require('./demo')
 
-if (location.pathname === '/') demo()
+if (location.pathname === '/' || location.pathname === '/index.html') demo()
 else if (location.pathname === '/filesystem.html') filesystem()
 else console.error('unexpected pathname')
 
